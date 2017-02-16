1b383b2aa0f7d5cbf306ae18b2ba0d4a434220ef
rafspiny <rafspiny@gmail.com>
2017-02-16 22:59:56 +0100

    Adding a crawler to find profiles in both the ini file and on filesystem.

---
+++ logic/profile_crawler.py
@@ -0,0 +1,98 @@
+import os.path as fs
+import os
+import re
+import model.models
+
+
+class ProfileSearcher():
+    """
+    Look for profiles
+    """
+
+    def __init__(self, ini_path=None, profile_dir=None):
+        # TODO maybe I should set ini_path and profile_dir to their respective linux, mac and windows value
+        self.present_on_fs = {}
+        self.present_in_profile_file = {}
+        self.recoverable = {}
+        self.inspect_profile(ini_path=ini_path)
+        self.inspect_fs(profile_dir=profile_dir)
+
+    def inspect_profile(self, ini_path):
+        """
+        Read the profiles.ini file
+        :return:
+        """
+        if ini_path is None:
+            raise Exception("No profiles.ini path has been specified")
+
+        if not fs.exists(ini_path):
+            raise Exception("File %s do not exists" % (ini_path))
+
+        lines = [line.strip() for line in open(ini_path, 'r')]
+        current_profile = None
+        for line in lines:
+            match = re.search("^\[Profile(\d+)\]$", line)
+            if match is not None:
+                # if the object is not null, add it to the list
+                if current_profile is not None:
+                    self.present_in_profile_file[current_profile.path] = current_profile
+
+                # extract the id and builds the object
+                current_id = match.group(1)
+                current_profile = model.models.Profile(profile_id=current_id)
+
+            match = re.search("^Name=(.*)", line)
+            if match is not None:
+                name = match.group(1)
+                current_profile.name = name
+
+            match = re.search("^IsRelative=(.*)", line)
+            if match is not None:
+                current_relative = int(match.group(1))
+                current_profile.isRelative = current_relative == 1
+
+            match = re.search("^Path=(.*)", line)
+            if match is not None:
+                current_path = match.group(1)
+                current_profile.path = current_path
+
+            match = re.search("^Default=(.*)", line)
+            if match is not None:
+                current_default = int(match.group(1))
+                current_profile.isDefault = current_default == 1
+
+        # finished reading lines, check the last profile
+        if current_profile is not None:
+            self.present_in_profile_file[current_profile.path] = current_profile
+
+    def inspect_fs(self, profile_dir):
+        """
+        Read the fs looking for firefox profiles
+        :return:
+        """
+        if profile_dir is None:
+            raise Exception("Mozilla directory is not specified")
+        if not fs.exists(profile_dir):
+            raise Exception("Dir %s does not exists" % profile_dir)
+
+        # Do not need to store the full profile object here, the path is enough
+        self.present_on_fs = [dir for dir in os.listdir(profile_dir) if
+                os.path.isdir(os.path.join(profile_dir, dir)) and re.search('\.', dir) is not None]
+
+    def compute_recoverable(self):
+        """
+        Compute the differences between the profiles from the fs and the one listed in the profiles.ini file
+        :return:
+        """
+        self.recoverable = set(self.present_on_fs) - set(self.present_in_profile_file)
+
+    def print(self):
+        print("From profiles: ")
+        for x in self.present_in_profile_file:
+            print("Profile: %s" % x)
+        print("From fs: ")
+        for x in self.present_on_fs:
+            print("Profile: %s" % x)
+        print("Recoverable: ")
+        for x in self.recoverable:
+            print("Profile: %s" % x)
---
+++ test/test_crawler.py
@@ -0,0 +1,144 @@
+from ddt import ddt, unpack, data
+import unittest
+import logic.profile_crawler
+import os
+import tempfile
+
+basedir = None
+unexisting_directory = None
+real_profile_path = None
+real_profiles_dir = None
+fake_profiles = ['s6zuh3tz.default', '2h2h4h.mr_x', '12k4k8.doctor_who']
+
+
+def create_fake_profile(profile):
+    """
+    Create a fake profile in the testing directory
+    :param profile: profile name
+    :return:
+    """
+    if not os.path.exists('%s%s%s' % (CrawlerTestCase.basedir, CrawlerTestCase.real_profiles_dir, profile)):
+        os.mkdir('%s%s%s' % (CrawlerTestCase.basedir, CrawlerTestCase.real_profiles_dir, profile))
+
+
+@ddt
+class CrawlerTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        """
+        Remove the directories for the fake profiles
+        :return:
+        """
+        super().tearDown()
+        for profile in fake_profiles:
+            if os.path.exists('%s%s%s' % (CrawlerTestCase.basedir, CrawlerTestCase.real_profiles_dir, profile)):
+                os.removedirs('%s%s%s' % (CrawlerTestCase.basedir, CrawlerTestCase.real_profiles_dir, profile))
+
+        if os.path.exists('%s%s%s' % (CrawlerTestCase.basedir, CrawlerTestCase.real_profiles_dir, 's6zuh3TZ.default')):
+            os.removedirs('%s%s%s' % (CrawlerTestCase.basedir, CrawlerTestCase.real_profiles_dir, 's6zuh3TZ.default'))
+
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Prepare the FS with all the directory needed for the test:
+        - a profile.ini file
+        - a profiles direcotry that wil hold the fake profiles
+        - create a placer that hold the name of a directory that does not exists.
+        :return:
+        """
+        cls.basedir = tempfile.mkdtemp(suffix='test', prefix='mozilla')
+        cls.unexisting_directory = '/bar'
+        tmp_unexisting_directory = '%s%s' % (cls.basedir, cls.unexisting_directory)
+
+        if os.path.exists(tmp_unexisting_directory):
+            os.removedirs(tmp_unexisting_directory)
+
+        # Make sure a bunch of fake profiles are created, along with a valid profiles.ini file
+        cls.real_profiles_dir = '/profiles/'
+        os.makedirs('%s%s' % (cls.basedir, cls.real_profiles_dir), exist_ok=True)
+        cls.real_profile_path = '/profiles/profiles.ini'
+        real_profile_fd = os.open('%s%s' % (cls.basedir, cls.real_profile_path), os.O_CREAT | os.O_WRONLY | os.O_TRUNC)
+
+        # content of fake profiles.ini
+        content = """
+[General]
+StartWithLastProfile=1
+
+[Profile0]
+Name=default
+IsRelative=1
+Path=s6zuh3tz.default
+Default=1
+"""
+
+        os.write(real_profile_fd, content.encode())
+        os.close(real_profile_fd)
+
+        if not os.path.exists('%s%s' % (cls.basedir, cls.real_profiles_dir)):
+            os.mkdir('%s%s' % (cls.basedir, cls.real_profiles_dir))
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Clean the temp directory
+        :return:
+        """
+        os.remove('%s%s' % (cls.basedir, cls.real_profile_path))
+        os.removedirs('%s%s' % (cls.basedir, cls.real_profiles_dir))
+        if os.path.exists(cls.basedir):
+            os.removedirs(cls.basedir)
+        super().tearDownClass()
+
+    @data(
+        (None, None, 'No profiles.ini path has been specified'),
+        ("/profiles.ini", None, 'File .*/profiles.ini do not exists'),
+        ('/profiles/profiles.ini', None, 'Mozilla directory is not specified'),
+        ('/profiles/profiles.ini', '/bar', 'Dir .* does not exists')
+    )
+    @unpack
+    def test_default_constructor_param(self, path, profile_folder, exception_message):
+        """
+        Verify that an exception is raised when invalid parameters are passed to the constructor
+        :param path: The path for the profiles.ini file
+        :param profile_folder: The profile direcotry in Firefox
+        :param exception_message: The expected error message
+        :return:
+        """
+        if path is not None:
+            path = '%s%s' % (CrawlerTestCase.basedir, path)
+        if profile_folder is not None:
+            profile_folder = '%s%s' % (CrawlerTestCase.basedir, profile_folder)
+
+        with self.assertRaisesRegex(Exception, exception_message):
+            logic.profile_crawler.ProfileSearcher(ini_path=path, profile_dir=profile_folder)
+
+    @data(
+        ('/profiles/profiles.ini', '/profiles/', ['s6zuh3tz.default'], set()),
+        ('/profiles/profiles.ini', '/profiles/', ['s6zuh3TZ.default'], {'s6zuh3TZ.default'}),
+        ('/profiles/profiles.ini', '/profiles/', ['s6zuh3tz.default', 's6zuh3TZ.default'], {'s6zuh3TZ.default'}),
+        ('/profiles/profiles.ini', '/profiles/', [], set()),
+        ('/profiles/profiles.ini', '/profiles/', ['s6zuh3tz.default', '2h2h4h.mr_x', '12k4k8.doctor_who'],
+         {'12k4k8.doctor_who', '2h2h4h.mr_x'})
+    )
+    @unpack
+    def test_compare_profiles(self, path, profile_folder, profiles_to_create, list_of_recoverables):
+        """
+        Test that the list of recoverable profiles is computed correctly
+        :param path: The path for the profiles.ini file
+        :param profile_folder: The profile direcotry in Firefox
+        :param profiles_to_create: List of fake profiles to create
+        :param list_of_recoverables: Expected set of recoverable profiles
+        :return:
+        """
+        if path is not None:
+            path = '%s%s' % (CrawlerTestCase.basedir, path)
+        if profile_folder is not None:
+            profile_folder = '%s%s' % (CrawlerTestCase.basedir, profile_folder)
+
+        for profile in profiles_to_create:
+            create_fake_profile(profile)
+
+        crawler = logic.profile_crawler.ProfileSearcher(ini_path=path, profile_dir=profile_folder)
+        crawler.compute_recoverable()
+        self.assertEqual(list_of_recoverables, crawler.recoverable)
